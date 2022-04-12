07fe7abbc0f7d168f72d375a6cd2728c55a4a25a
PauloNevesParone <paulo.neves@parone.com.br>
2022-04-11 23:09:06 -0300

    [CU-22hc19v] creates menu with menu items

---
+++ components/VMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="menu">
+    <menu-item
+      v-for="(item, index) in menuItems"
+      :key="`${index}-${item}`"
+      :label="item"
+    />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'VMenu',
+  data: () => ({
+    menuItems: [
+      'Potfolio',
+      'About Us',
+      'Members',
+      'Contact Us'
+    ]
+  })
+}
+</script>
+
+<style scoped lang="scss">
+  .menu {
+    display: flex;
+    justify-content: space-between;
+    gap: 48px;
+  }
+</style>
