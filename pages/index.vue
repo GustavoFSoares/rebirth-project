6c1610423d216f14f4c8bad14988d2a2a9c1b47c
PauloNevesParone <paulo.neves@parone.com.br>
2022-04-19 14:17:34 -0300

    new header

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="main">
     <v-header />
-
-    <v-section title="Title1">
-      <service-item
-        title="title"
-        content="Chuck Norris threw a grenade and killed 50 people. Then it exploded."
-      />
-    </v-section>
-
-    <v-section title="Title2" type="light">
-      <service-item
-        title="title"
-        content="Chuck Norris threw a grenade and killed 50 people. Then it exploded."
-      />
-    </v-section>
   </div>
 </template>
 
