5d7f2c0965092d8176e4ec21e80d1424544c06f8
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-04-24 17:24:33 -0300

    [CU-22hc122] created about-us page

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,31 @@
 
     <our-services />
 
+    <about-us />
+
     <v-footer />
   </div>
 </template>
 
 <script>
 import OurServices from '~/pages/partials/OurServices'
+import AboutUs from '~/pages/partials/AboutUs'
 import VFooter from '~/pages/partials/VFooter'
 
 export default {
   name: 'IndexPage',
   components: {
     OurServices,
+    AboutUs,
     VFooter
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  //
+.main {
+  &:first-child {
+    margin-top: calc(1rem + 72px);
+  }
+}
 </style>
