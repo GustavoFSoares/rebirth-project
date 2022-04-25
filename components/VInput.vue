4ef3acd61736ba714bca73ab15d48dabd49d7913
PauloNevesParone <paulo.neves@parone.com.br>
2022-04-24 22:49:33 -0300

    [CU-22hc33m] adds input layout

--- components/VInput.vue
+++ components/VInput.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="form-input">
-    <label>
-      <span class="form-input__label">
+    <label class="flex flex-column flex-gap-1">
+      <span class="form-input__label rb-font-size-16 rb-font-weight-bold rb-text-color-2">
         {{ label }}
       </span>
       <input
+        v-if="type !== 'textarea'"
         v-model="value"
         :type="type"
-        class="form-input__input"
+        class="form-input__input rb-text-color-3 rb-font-weight-bold"
         :placeholder="placeholder"
         @input="updateValue"
         @blur="blur"
       >
+      <textarea
+        v-else
+        v-model="value"
+        :type="type"
+        class="form-input__input rb-text-color-3 rb-font-weight-bold"
+        :placeholder="placeholder"
+        @input="updateValue"
+        @blur="blur"
+      />
     </label>
     <transition>
-      <div v-if="error.$error" class="form-input__message">
-        <span v-if="!error.required" class="form-input__message--error">
-          {{ errorMessage }}
+      <div v-if="error.$error" class="form-input__message mw-2">
+        <span
+          v-if="!error.required || !error.email"
+          class="form-input__message--error rb-text-color-primary rb-font-size-12 flex flex-start flex-gap-1 flex-center-center"
+        >
+          <error-icon class="form-input__message-icon" /> {{ errorMessage }}
         </span>
       </div>
     </transition>
@@ -27,6 +40,9 @@
 
 export default {
   name: 'VInput',
+  components: {
+    ErrorIcon: () => import('@/assets/images/icons/error-icon.svg?inline')
+  },
   props: {
     label: {
       type: String,
@@ -61,4 +77,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  .form-input {
+    &__input {
+      border: none;
+      padding: 5px 10px;
+      border-radius: 4px;
+      line-height: 2;
+
+      &:focus-visible,
+      &:active {
+        outline: unset;
+      }
+    }
+    &__message-icon {
+      max-width: 20px;
+    }
+  }
 </style>
