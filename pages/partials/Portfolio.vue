af3a3912a83b7cfdbd0edc6a6a4e298de5801294
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-04-26 23:04:10 -0300

    [CU-22hc11z] started postfolio page

---
+++ pages/partials/Portfolio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-section :title="$t('section.portfolio.title')">
+      <template #container>
+        <h1 style="background: white">
+          123
+        </h1>
+      </template>
+    </v-section>
+  </div>
+</template>
+
+<script>
+// import cube from '@/helpers/cube-portifolio-helper'
+
+export default {
+  name: 'PPortfolio'
+}
+</script>
