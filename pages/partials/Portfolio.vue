82324c7a083f795f14b5a8c815a351f261d8cff6
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-04-27 23:12:16 -0300

    [CU-22hc11z] created portfolio component

--- pages/partials/Portfolio.vue
+++ pages/partials/Portfolio.vue
@@ -1,103 +1,113 @@
 <template>
   <v-section class="rb-mb-64" :title="$t('section.portfolio.title')">
     <template #container>
-      <div id="js-grid-mosaic-flat" class="cbp cbp-l-grid-mosaic-flat">
+      <div class="portfolio-container">
         <div
           v-for="(photo, index) in images"
           :key="index"
-          class="cbp-item"
-          :class="photo.groupBy"
-        >
-          <a :href="photo.url" class="cbp-caption cbp-lightbox">
-            <div class="cbp-caption-defaultWrap">
-              <img :src="photo.url" :alt="'photo.name'">
-            </div>
-            <!-- <div class="cbp-caption-activeWrap" />
-            <div class="cbp-l-caption-alignCenter center-block">
-              <div class="cbp-l-caption-body">
-                <div class="plus" />
-                <h5 class="text-white mb-1">
-                  Ampliar
-                </h5>
-                <p class="text-white">
-                  {{ 'photo.description' }}
-                </p>
-              </div>
-            </div> -->
-          </a>
-        </div>
+          class="portfolio-item"
+          :style="`background-image: url(${photo.url})`"
+        />
       </div>
     </template>
   </v-section>
 </template>
 
 <script>
-import cube from '@/helpers/cube-portifolio-helper'
-
 export default {
   name: 'PPortfolio',
   computed: {
     images () {
-      const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-      const maxImages = 20
-      const totalImagesAvailable = 28
+      const numbers = [...new Array(28)]
+      const imageCount = numbers.map((_, image) => ({ url: `/portifolio/${image + 1}.webp` }))
 
-      while (numbers.length !== maxImages) {
-        const random = Math.floor(Math.random() * totalImagesAvailable)
-
-        if (!numbers.includes(random) && random !== 0) {
-          numbers.push(random)
-        }
-      }
-
-      console.log(numbers)
-      const imageCount = numbers.map(image => ({ url: `portifolio/${image}.webp` }))
-
-      return imageCount
+      return this.shuffle(imageCount)
     }
   },
-  mounted () {
-    this.startCubePortfolio()
-  },
   methods: {
-    startCubePortfolio () {
-      cube('#js-grid-mosaic-flat', {
-        filters: '#js-filters-mosaic-flat',
-        layoutMode: 'grid',
-        sortByDimension: true,
-        mediaQueries: [
-          // { width: 1921, cols: 6 },
-          { width: 1440, cols: 6 },
-          // { width: 1920, cols: 5 },
-          // { width: 1400, cols: 4 },
-          { width: 991, cols: 4 },
-          { width: 767, cols: 2 },
-          { width: 480, cols: 1, options: { caption: '', gapHorizontal: 15, gapVertical: 15 } }
-        ],
-        defaultFilter: '*',
-        animationType: 'fadeOutTop',
-        gapHorizontal: 0,
-        gapVertical: 0,
-        gridAdjustment: 'responsive',
-        caption: 'zoom',
-        displayType: 'fadeIn',
-        displayTypeSpeed: 100
-
-        // lightbox
-        // lightboxDelegate: '.cbp-lightbox',
-        // lightboxGallery: true,
-        // lightboxTitleSrc: 'data-title',
-        // lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>'
-
-        // plugins: {
-        //   loadMore: {
-        //     element: '#js-loadMore-mosaic-flat',
-        //     action: 'click',
-        //     loadItems: 3
-        //   }
-        // }
-      })
+    shuffle (imageList) {
+      return imageList.sort(() => Math.random() - 0.5)
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.portfolio-container {
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  overflow: hidden;
+
+  @include media("mobile", "max") {
+    max-height: 80vh;
+    overflow-y: auto;
+  }
+
+  > div {
+    background: #ccc;
+    flex: 1 0 50px;
+    height: 200px;
+    transition: all ease 1s;
+    border-radius: 2px;
+    background-position: center 15%;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    @include media("mobile", "max") {
+      background-position: center center;
+      min-width: 200px;
+    }
+
+    &:before {
+      content: ' ';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      min-height: 100%;
+
+      background: $secondary;
+      opacity: 0;
+      transition: opacity ease-in .2s;
+    }
+
+    &:hover {
+      position: relative;
+      transform: scale(1.03);
+      cursor: pointer;
+      box-shadow: 0 0 15px 10px rgba(0, 0, 0, .3);
+      overflow: hidden;
+
+      &:before {
+        opacity: 0.2;
+      }
+    }
+
+    &:nth-child(2n+1) {
+      min-width: 25%;
+    }
+    &:nth-child(3n+1) {
+      min-width: 10%;
+    }
+    &:nth-child(5n+1) {
+      min-width: 30%;
+    }
+    &:nth-child(even) {
+      min-width: 15%;
+    }
+
+    @include media("mobile", "max") {
+      &:nth-child(2n+1) {
+        min-width: 40%;
+      }
+      &:nth-child(3n+1) {
+        min-width: 60%;
+      }
+      &:nth-child(even) {
+        min-width: 20%;
+      }
+    }
+  }
+}
+</style>
