e5e70e6bd5158326880e6396c183566052f9ede0
Gustavo Soares <gustavo.fe.soares@gmail.com>
2022-04-28 22:21:13 -0300

    [CU-22hc1u1] created carousel component

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/vuelidate',
-    { src: '@/plugins/vue-carousel.js', mode: 'client' }
+    { src: '@/plugins/vue-carousel', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
